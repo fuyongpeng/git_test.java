ac3c51dd27b746c896cbc43c96ad979ed7eec1e7
fyp <690703052@qq.com>
2022-01-03 21:05:49 +0800

    hot_fix_commit2_master

--- src/main/java/a/ab.java
+++ src/main/java/a/ab.java
@@ -5,6 +5,6 @@ public class ab {
         System.out.println("hello_git");
         System.out.println("hello_git2");
         System.out.println("hello_git3");
-        System.out.println("hello_git4");
+        System.out.println("hello_git4_master");
     }
 }
